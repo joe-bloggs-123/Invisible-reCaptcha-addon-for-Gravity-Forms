446c3921ec90055f48456600470d1d77a06b49fa
Rob Marshall <rob@fox.agency>
2019-05-28 12:10:28 +0100

    Removed wierd duplicate

--- js/frontend.js
+++ js/frontend.js
@@ -8,64 +8,6 @@ grecaptcha.ready(function() {
 
         if (null === holder) continue
         holder.innerHTML = ''
-
-        grecaptcha.ready(function() {
-            for (var i = 0; i < document.forms.length; ++i) {
-                var form = document.forms[i]
-
-                var holder = form.querySelector('.gf-recaptcha-div')
-
-                if (null === holder) continue
-                holder.innerHTML = ''
-                ;(function(frm) {
-                    // This sets everything up for the form call
-                    var holderId = grecaptcha.render(holder, {
-                        sitekey: key,
-                        size: 'invisible',
-                        badge: 'inline',
-                    })
-
-                    // Here the form is submitted, using everything from, above
-                    frm.addEventListener('submit', function(evt) {
-                        evt.preventDefault()
-                        // Execute and get token
-                        grecaptcha
-                            .execute(holderId, { action: form.id })
-                            .then(function(token) {
-                                // Take token and pass to server
-                                tellServer(token, frm)
-                            })
-                    })
-                })(form)
-            }
-        })
-
-        function tellServer(token, frm) {
-            var data = {
-                action: 'check_google_token_request',
-                token: token,
-                security: gfGoogleCaptchaScriptFrontend_strings.security,
-            }
-
-            axios
-                .post(
-                    gfGoogleCaptchaScriptFrontend_strings.ajaxurl,
-                    Qs.stringify(data)
-                )
-                .then(function(response) {
-                    if (response.data.success) {
-                        frm.submit()
-                    } else {
-                        console.log('Is a bot')
-                    }
-                    console.log(response)
-                })
-                .catch(function(error) {
-                    // handle error
-                    console.log(error)
-                })
-        }
-
         ;(function(frm) {
             // This sets everything up for the form call
             var holderId = grecaptcha.render(holder, {
@@ -75,7 +17,7 @@ grecaptcha.ready(function() {
             })
 
             // Here the form is submitted, using everything from, above
-            frm.onsubmit(function(evt) {
+            frm.addEventListener('submit', function(evt) {
                 evt.preventDefault()
                 // Execute and get token
                 grecaptcha
@@ -88,3 +30,25 @@ grecaptcha.ready(function() {
         })(form)
     }
 })
+
+function tellServer(token, frm) {
+    var data = {
+        action: 'check_google_token_request',
+        token: token,
+        security: gfGoogleCaptchaScriptFrontend_strings.security,
+    }
+
+    axios
+        .post(gfGoogleCaptchaScriptFrontend_strings.ajaxurl, Qs.stringify(data))
+        .then(function(response) {
+            if (response.data.success) {
+                frm.submit()
+            } else {
+                console.log('Is a bot')
+            }
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error)
+        })
+}
